dd1e20cb8e08114e641776114a221b14ebd3437b
codeerror90 <79945630+codeerror90@users.noreply.github.com>
2022-08-30 08:51:34 -0500

    Update owner-exec2.js

--- plugins/owner-exec2.js
+++ plugins/owner-exec2.js
@@ -1,4 +1,4 @@
-import cp, { exec as _exec } from 'child_process'
+/*import cp, { exec as _exec } from 'child_process'
 import { promisify } from 'util'
 let exec = promisify(_exec).bind(cp)
 let handler = async (m, { conn, isOwner, command, text }) => {
@@ -19,3 +19,4 @@ handler.customPrefix = /^[$]/
 handler.command = new RegExp
 handler.rowner = true
 export default handler
+*/
